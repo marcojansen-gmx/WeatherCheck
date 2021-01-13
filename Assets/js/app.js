8130cb0768ba727b817e1351367a30a2655510d6
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-01-13 15:19:18 +0800

    defactor setAttribute error of null

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,65 +1,67 @@
 // Dom Manipulation and other code go here 
-const cityForm = document.querySelector('.enter-location');
+const cityForm = document.querySelector('.enter-city');
 //elements to update main card
 const card = document.querySelector('.card');
-const details = document.querySelector('.details')
+const details = document.querySelector('.details');
 //graphic elements to be updated
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
-
-
 
 //func to update  UI
 const updateUI = (data) => {
     // const cityDetails = data.cityDetails;
     // const weatherDetails = data.weatherDetails;
 
-    // destrcuturing the data properties
-    const { cityDetails, weatherDetails } = data;
+  // destructure properties
+  const { cityDetails, weatherDetails } = data;
 
-    // update the details on the card 
-    details.innerHTML = `
-        <h5 class="my-3">${cityDetails.EnglishName}</h5>
-        <div class="my-3">${weatherDetails.WeatherText}</div>
-        <div class="display-4 my-4">
-            <span>${weatherDetails.Temperature.Metric.Value}</span>
-            <span>&deg;C</span>
-        </div>
-    `;
+  // update the details on the card
+  details.innerHTML = `
+    <h5 class="my-3">${cityDetails.EnglishName}</h5>
+    <div class="my-3">${weatherDetails.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weatherDetails.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>
+  `;
 
-    // update the day  or night display
-    let timeSrc = null;
-    if (weatherDetails.IsDayTime){
-        timeSrc = 'Assets/graphics/images/day.svg';
-    }else{
-        timeSrc = 'Assets/graphics/images/night.svg';
-    }
-    time.setAttribute('src', timeSrc);
+  const iconSrc = `Assets/graphics/icons/${weatherDetails.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
+  // update the day  or night display
+  let timeSrc = null;
+  if(weatherDetails.IsDayTime){
+    timeSrc = 'Assets/graphics/images/day.svg';
+  } else {
+    timeSrc = 'Assets/graphics/images/night.svg';
+  }
+  time.setAttribute('src', timeSrc);
 
-    // remove the class of d-none if it is present
-    if(card.classList.contains('d-none')){
-        card.classList.remove('d-none');
-    }
+  // remove the class of d-none if it is present
+  if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+  }
 };
 
 const updateCity = async (city) => {
-    
-    const cityDetails = await getCity(city);
-    const weatherDetails = await getWeather(cityDetails.Key);
 
-    return { cityDetails, weatherDetails, };
+  const cityDetails = await getCity(city);
+  const weatherDetails = await getWeather(cityDetails.Key);
+  return { cityDetails, weatherDetails };
+
 };
 
 cityForm.addEventListener('submit', e => {
-    // prevent detault action so it doesn't refresh page
-    e.preventDefault();
-    // getting city value
-    const city =  cityForm.city.value.trim();
-    // clear form
-    cityForm.reset();
-    
-    //update the User Interface with the new City
-    updateCity(city)
+  // prevent detault action so it doesn't refresh page
+  e.preventDefault();
+  
+   // getting city value
+  const city = cityForm.city.value.trim();
+  // clear form
+  cityForm.reset();
+
+  //update the User Interface with the new City
+  updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
--- Assets/js/weathercheck.js
+++ Assets/js/weathercheck.js
@@ -1,28 +1,30 @@
-const key = '2YG8gYCt8OGBLka1i2lyjA80yj9Z5g2p';
+const key = 'q5Xsdy5sqmdb4MwpDtDT9cpWPAMSRoAy';
 
 // function to get current weather information for city
 const getWeather = async (id) => {
-const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-const query = `${id}?apikey=${key}`;
-
-const response = await fetch(base + query);
-const data = await response.json();
-
-return data[0];
-
-};
-
-// function to get id for city - set city as paramater  - to obtain weather data 
-const getCity = async (city) => {
+  
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+  
+  // function to get id for city - set city as paramater  - to obtain weather data
+  const getCity = async (city) => {
+  
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
     // build query to get city id using API key and city input
     const query = `?apikey=${key}&q=${city}`;
     // fetch resource (city id) and combine base and query together
     const response = await fetch(base + query);
     const data = await response.json();
-    console.log(data[0]);
+    // console.log(data[0]);
     return data[0];
-};
+  };
 
 // kan uitvee
 // getCity('Manchester').then(data => {
