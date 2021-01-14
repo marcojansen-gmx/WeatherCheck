db6b52deb526d6fdd4ee314bc54051bf6d38714b
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-01-14 13:51:21 +0800

    fixed ternary operator

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -11,57 +11,60 @@ const icon = document.querySelector('.icon img');
 const updateUI = (data) => {
     // const cityDetails = data.cityDetails;
     // const weatherDetails = data.weatherDetails;
-
-  // destructure properties
-  const { cityDetails, weatherDetails } = data;
-
-  // update the details on the card
-  details.innerHTML = `
+    
+    // destructure properties
+    const { cityDetails, weatherDetails } = data;
+    
+    // update the details on the card
+    details.innerHTML = `
     <h5 class="my-3">${cityDetails.EnglishName}</h5>
     <div class="my-3">${weatherDetails.WeatherText}</div>
     <div class="display-4 my-4">
-      <span>${weatherDetails.Temperature.Metric.Value}</span>
-      <span>&deg;C</span>
+    <span>${weatherDetails.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
     </div>
-  `;
-
-  const iconSrc = `Assets/graphics/icons/${weatherDetails.WeatherIcon}.svg`;
-  icon.setAttribute('src', iconSrc);
-  
-  // update the day  or night display
-  let timeSrc = weatherDetails.IsDayTime ? 'Assets/graphics/images/day.svg' ; 'Assets/graphics/images/night.svg';
-//   if(weatherDetails.IsDayTime){
-//     timeSrc = 'Assets/graphics/images/day.svg';
-//   } else {
-//     timeSrc = 'Assets/graphics/images/night.svg';
-//   }
-//   time.setAttribute('src', timeSrc);
-
-  // remove the class of d-none if it is present
-  if(card.classList.contains('d-none')){
-    card.classList.remove('d-none');
-  }
-};
-
-const updateCity = async (city) => {
-
-  const cityDetails = await getCity(city);
-  const weatherDetails = await getWeather(cityDetails.Key);
-  return { cityDetails, weatherDetails };
-
-};
-
-cityForm.addEventListener('submit', e => {
-  // prevent detault action so it doesn't refresh page
-  e.preventDefault();
-  
-   // getting city value
-  const city = cityForm.city.value.trim();
-  // clear form
-  cityForm.reset();
-
-  //update the User Interface with the new City
-  updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
-});
+    `;
+    
+    const iconSrc = `Assets/graphics/icons/${weatherDetails.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+    // update the day  or night display  
+    let timeSrc = weatherDetails.IsDayTime ? 'Assets/graphics/images/day.svg' : 'Assets/graphics/images/night.svg';
+    time.setAttribute('src', timeSrc);
+    
+    // let timeSrc = null;          
+    //   if(weatherDetails.IsDayTime){
+    //         timeSrc = 'Assets/graphics/images/day.svg';
+    //       } else {
+    //             timeSrc = 'Assets/graphics/images/night.svg';
+    //           }
+            
+            // remove the class of d-none if it is present
+            if(card.classList.contains('d-none')){
+                card.classList.remove('d-none');
+            }
+        };
+        
+        const updateCity = async (city) => {
+            
+            const cityDetails = await getCity(city);
+            const weatherDetails = await getWeather(cityDetails.Key);
+            return { cityDetails, weatherDetails };
+            
+        };
+        
+        cityForm.addEventListener('submit', e => {
+            // prevent detault action so it doesn't refresh page
+            e.preventDefault();
+            
+            // getting city value
+            const city = cityForm.city.value.trim();
+            // clear form
+            cityForm.reset();
+            
+            //update the User Interface with the new City
+            updateCity(city)
+            .then(data => updateUI(data))
+            // console.log(data[0])
+            .catch(err => console.log(err));
+        });
